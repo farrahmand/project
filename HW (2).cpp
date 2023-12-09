d683cd8ba37147ef7eadffd4ce144a9f98e91aa0
farrahmand <153393148+farrahmand@users.noreply.github.com>
2023-12-10 01:18:53 +0330

    Add files via upload

---
+++ HW (2).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+ int main() {
+     float x, y, z;
+     int result0 , result1 ;
+     count << "please enter x : " ;
+     cin >> x ;
+     count << "please enter y : " ; 
+     cin >> y ;
+     count << "please enter z : " ; 
+     cin >> z ;
+     result0 = (x - y) * (x + z) ;
+     result1 = pow(x + y + z , z) + 1 ;
+     cout << (flout) result0 / result1 ;
+}
